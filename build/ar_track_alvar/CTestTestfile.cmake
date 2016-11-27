dcbd7872c8029d03b00134397bb3e20dba1c1f53
ych09041 <ych09041@gmail.com>
2016-11-27 14:10:25 -0800

    remove build and devel from repo tracking

--- build/ar_track_alvar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/team15/ee206akitchen/src/ar_track_alvar
-# Build directory: /home/team15/ee206akitchen/build/ar_track_alvar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/baxterkitchen/CMakeFiles/baxter_examples_gencfg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/baxter_examples_gencfg"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/baxter_examples_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
