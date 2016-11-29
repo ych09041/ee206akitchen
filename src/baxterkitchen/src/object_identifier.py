6316d464f9bd16540ebc64fe38d563df893bb6e8
ych09041 <ee106a-acu@imail.EECS.Berkeley.EDU>
2016-11-28 17:07:11 -0800

    some testing

--- src/baxterkitchen/src/object_identifier.py
+++ src/baxterkitchen/src/object_identifier.py
@@ -15,16 +15,17 @@ import baxterkitchen.msg
 listener = None
 
 ar_tags = {}
-ar_tags['knife'] = 'ar_marker_12'
+ar_tags['knife'] = 'test_frame'
 ar_tags['board'] = 'ar_marker_2'
 ar_tags['cucumber'] = 'ar_marker_4'
-ar_tags['eye'] = 'usb_cam'
+ar_tags['usb_cam'] = 'usb_cam'
 ar_tags['naval'] = 'naval'
+ar_tags['test_frame'] = 'test_frame'
+ar_tags['test_frame2'] = 'test_frame2'
 
 
 
-
-def update_inventory(ar_tags):
+def update_inventory():
 
     pub = rospy.Publisher('inventory', Inventory, queue_size=10)
     listener = tf.TransformListener()
@@ -33,17 +34,18 @@ def update_inventory(ar_tags):
     while not rospy.is_shutdown():
         print '==================='
         message = Inventory()
+        try:
+            (trans1, rot1) = listener.lookupTransform(ar_tags['naval'], ar_tags['knife'], rospy.Time(0))
+            message.knife_translation = trans1
+            message.knife_rotation = rot1
+            message.knife_seen = True
         
-        (trans1, rot1) = listener.lookupTransform(ar_tags['knife'], ar_tags['naval'], rospy.Time(0))
-        message.knife_translation = trans1
-        message.knife_rotation = rot1
-        message.knife_seen = True
-        
-        (trans2, rot2) = listener.lookupTransform(ar_tags['cucumber'], ar_tags['naval'], rospy.Time(0))
-        message.cucumber_translation = trans2
-        message.cucumber_rotation = rot2
-        message.cucumber_seen = True
-        
+           # (trans2, rot2) = listener.lookupTransform(ar_tags['cucumber'], ar_tags['naval'], rospy.Time(0))
+           # message.cucumber_translation = trans2
+           # message.cucumber_rotation = rot2
+          #  message.cucumber_seen = True
+        except tf.LookupException or tf.ExtrapolationException or rospy.ServiceException as e:
+            print e
 
         pub.publish(message)
 
