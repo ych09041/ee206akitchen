7916c21c090b8c1d06bace0fe7c3573ca6b89ade
Class_Account <cs199-dec@imail.EECS.Berkeley.EDU>
2016-11-30 16:58:20 -0800

    Use moveit_python

--- src/baxterkitchen/src/plan_test2.py
+++ src/baxterkitchen/src/plan_test2.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 
 import roslib
+import sys
 import rospy
+import moveit_commander
+from moveit_msgs.msg import OrientationConstraint, Constraints
+from geometry_msgs.msg import PoseStamped
+from baxter_interface import gripper as baxter_gripper
 from moveit_python import *
 
 # Brings in the SimpleActionClient
 import actionlib
 
 # goal message and the result message.
-import baxterkitchen.msg
-from baxterkitchen.msg import Inventory
-from baxter_interface import gripper as baxter_gripper
 
 def pick_client(si, px, py, pz):
     client = actionlib.SimpleActionClient('pick_server', baxterkitchen.msg.PickAction)
@@ -22,9 +24,25 @@ def pick_client(si, px, py, pz):
 
 
 if __name__ == '__main__':
+	# moveit_commander.roscpp_initialize(sys.argv)
 	rospy.init_node("moveit_py")
+	# scene = moveit_commander.planning_scene_interface.PlanningSceneInterface()
+	# scene.remove_world_object("cube1")
+	# goal_1 = PoseStamped()
+	# goal_1.pose.position.x = 0.4
+	# goal_1.pose.position.y = 0.0
+	# goal_1.pose.position.z = 0.0
+
+	# #Orientation as a quaternion
+	# goal_1.pose.orientation.x = 0.0
+	# goal_1.pose.orientation.y = -1.0
+	# goal_1.pose.orientation.z = 0.0
+	# goal_1.pose.orientation.w = 0.0
+	# scene.add_box("cube3",goal_1)
 	print("here")
 	p = PlanningSceneInterface("base")
-	p.addCube("my_cube", 0.1, 1, 0, 0.5)
+	p.addCube("cube1", 0.1, 2, 0, 0.5)
+	p.addCube("cube2", 0.1, 2, 2, 0.5)
+
 
 
